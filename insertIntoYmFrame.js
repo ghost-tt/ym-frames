63aed654730410e9bd6c58936bbfd27b69d06714
Ashish Sharma <Ashishbhardwaj727@gmail.com>
2021-06-05 17:34:10 +0530

    insert desktop and mobile media query

--- insertIntoYmFrame.js
+++ insertIntoYmFrame.js
@@ -5,28 +5,27 @@
         modularBars.type = 'text/javascript';
         modularBars.src = 'https://ghost-tt.github.io/ym-frames/addFrames.js';
         iFrameHead.appendChild(modularBars);
-        
-        injectDynamicCss();
+        injectDynamicCssToParent();
+        injectDynamicCssToChild();
     } catch(e) {
         console.error("failed while inserting to iFrame", e);
     }
 })();
 
 
-function injectDynamicCss() {
+function injectDynamicCssToChild() {
     var isMobile = checkIfMobile();
-    if(!isMobile) {
-        document.getElementById("ymFrameHolder").style.width = "440px";
-    } {
-        document.getElementById("ymFrameHolder").style.width = "100%";
-    }
-
+//     if(!isMobile) {
+//         document.getElementById("ymFrameHolder").style.width = "440px";
+//     } {
+//         document.getElementById("ymFrameHolder").style.width = "100%";
+//     }
     var ymFrameHead = window.frames["ymIframe"].document.getElementsByTagName("head")[0];  
     var modularStyles = document.createElement('style');
     modularStyles.type = 'text/css';
 
     var css = '#chatBoxMainContainer.message-icons.live-chat { margin: 3rem 0 0 3rem; }';
-    if (modularStyles.styleSheet){
+    if (modularStyles.styleSheet) {
       modularStyles.styleSheet.cssText = css;
     } else {
       modularStyles.appendChild(document.createTextNode(css));
@@ -34,6 +33,20 @@ function injectDynamicCss() {
     ymFrameHead.appendChild(modularStyles);
 }
 
-function checkIfMobile() {
-    return ( ( window.innerWidth <= 800 ) && ( window.innerHeight <= 600 ) );
+function injectDynamicCssToParent() {
+    var parentCssHead = document.head || document.getElementsByTagName('head')[0],
+    var parentStyles = document.createElement('style');
+    parentStyles.type = 'text/css';
+
+    var parentCssStyles = '#ymFrameHolder { width: 440px; } @media only screen and (min-width: 768px) { #ymFrameHolder { width: 100%; }}';
+    if (parentStyles.styleSheet) {
+      parentStyles.styleSheet.cssText = css;
+    } else {
+      parentStyles.appendChild(document.createTextNode(css));
+    }
+    parentCssHead.appendChild(style);
 }
+
+// function checkIfMobile() {
+//     return ( ( window.innerWidth <= 800 ) && ( window.innerHeight <= 600 ) );
+// }
