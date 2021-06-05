444d2191742a6625b4deac92a1ed58e5b6d0d0b1
Ashish Sharma <Ashishbhardwaj727@gmail.com>
2021-06-05 15:49:32 +0530

    - reverting last commit

--- addFrames.js
+++ addFrames.js
@@ -1,18 +1,5 @@
 (function addFrames () {
   try {
-    var css = '#chatBoxMainContainer.message-icons.live-chat { margin: 3rem 0 0 3rem; }',
-    head = document.head || document.getElementsByTagName('head')[0],
-    style = document.createElement('style');
-
-    head.appendChild(style);
-
-    style.type = 'text/css';
-    if (style.styleSheet){
-      style.styleSheet.cssText = css;
-    } else {
-      style.appendChild(document.createTextNode(css));
-    }
-    
     document.querySelector('#chatContainer').insertAdjacentHTML(
       'afterbegin',
       `<div>
