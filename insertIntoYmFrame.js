03f090954dfcc4fde371e67420b329551265aa8f
Ashish Sharma <Ashishbhardwaj727@gmail.com>
2021-06-05 15:05:35 +0530

    Create insertIntoYmFrame.js

---
+++ insertIntoYmFrame.js
@@ -0,0 +1,11 @@
+(function injectJS() {
+    try {
+      var iFrameHead = window.frames["ymIframe"].document.getElementsByTagName("head")[0];         
+      var modularBars = document.createElement('script');
+      modularBars.type = 'text/javascript';
+      modularBars.src = 'https://ghost-tt.github.io/ym-frames/addFrames.js';
+      iFrameHead.appendChild(modularBars);
+    } catch(e) {
+      console.error("failed while inserting to iFrame", e);
+    }
+})();
