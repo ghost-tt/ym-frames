aaf94971bebb64e2ba79c3bdadb02acea2f5a981
Ashish Sharma <Ashishbhardwaj727@gmail.com>
2021-06-05 15:57:32 +0530

    reverting back to old commit

--- insertIntoYmFrame.js
+++ insertIntoYmFrame.js
@@ -1,19 +1,5 @@
 (function injectJS() {
     try {
-        document.getElementById("ymFrameHolder").style.width = "440px";
-       
-        var css = '#chatBoxMainContainer.message-icons.live-chat { margin: 3rem 0 0 3rem; }',
-        head = document.head || document.getElementsByTagName('head')[0],
-        style = document.createElement('style');
-        head.appendChild(style);
-
-        style.type = 'text/css';
-        if (style.styleSheet){
-          style.styleSheet.cssText = css;
-        } else {
-          style.appendChild(document.createTextNode(css));
-        }
-        
         var iFrameHead = window.frames["ymIframe"].document.getElementsByTagName("head")[0];         
         var modularBars = document.createElement('script');
         modularBars.type = 'text/javascript';
