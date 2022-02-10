4be563f4f493dfb4d1341e5699fa9be75c54f635
Ashish Sharma <Ashishbhardwaj727@gmail.com>
2022-02-10 17:48:41 +0530

    adding unminify code to debug

--- addFrames.js
+++ addFrames.js
@@ -1,1 +1,51 @@
-(function addFrames(){try{window.onresize=function(event){var frameHeight=window.innerHeight-95-48;document.getElementById("chatBoxMainContainer").style.height=`${frameHeight}px`};document.querySelector('#chatContainer').insertAdjacentHTML('afterbegin',`<div class="mod-head-side-bar-container"><div class="overflow-container-sidebar"><ul class="sidebar-wrapper"><div onclick="dynamicHeaderTitle(event)"class="side-bar-icon-box"title="title 1"><img class="sidebar-img-icon"src="https://cdn.yellowmessenger.com/LS9XYE9urVcj1605084428877.png"/></div><div onclick="dynamicHeaderTitle(event)"class="side-bar-icon-box"title="title 2"><img class="sidebar-img-icon"src="https://cdn.yellowmessenger.com/GnxBWRVeIm6i1622800692538.png"/></div><div onclick="dynamicHeaderTitle(event)"class="side-bar-icon-box"title="title 3"><img class="sidebar-img-icon"src="https://cdn.yellowmessenger.com/4dnzmSDugrwW1622800597860.png"/></div><div onclick="dynamicHeaderTitle(event)"class="side-bar-icon-box"title="title 4"><img class="sidebar-img-icon"src="https://cdn.yellowmessenger.com/peOgkPdE8n0A1622800752931.png"/></div><div onclick="dynamicHeaderTitle(event)"class="side-bar-icon-box"title="title 5"><img class="sidebar-img-icon"src="https://cdn.yellowmessenger.com/FPloPRu3LmfA1622800812350.png"/></div><div onclick="dynamicHeaderTitle(event)"class="side-bar-icon-box"title="title 6"><img class="sidebar-img-icon"src="https://cdn.yellowmessenger.com/sULHcUPQH5RL1622800858305.png"/></div><div onclick="dynamicHeaderTitle(event)"class="side-bar-icon-box"title="title 7"><img class="sidebar-img-icon"src="https://cdn.yellowmessenger.com/VLla29GmwVpi1622800914321.png"/></div><div onclick="dynamicHeaderTitle(event)"class="side-bar-icon-box"title="title 8"><img class="sidebar-img-icon"src="https://cdn.yellowmessenger.com/o2BBDya1HiQT1622800956549.png"/></div><div onclick="dynamicHeaderTitle(event)"class="side-bar-icon-box"title="title 9"><img class="sidebar-img-icon"src="https://cdn.yellowmessenger.com/g6Fdrr5NeUj01622800983140.png"/></div></ul></div><div class="overflow-container-header"><ul class="header-wrapper"><div class="header-title"id="main-header-title">Header</div><div class="icons-box"><span class="header-bar-icon-box"title="Minimize">-</span><span class="header-bar-icon-box"title="Close">x</span></div></ul></div></div>`)}catch(e){console.log("failing while trying to insert add frame script",e)}})();function dynamicHeaderTitle(e){e.preventDefault();var title=e.srcElement.getAttribute("title");document.getElementById("main-header-title").textContent=title}
+(function addFrames() {
+  try {
+    window.onresize = function (event) {
+      var frameHeight = window.innerHeight - 104 - 48;
+      document.getElementById(
+        "chatBoxMainContainer"
+      ).style.height = `${frameHeight}px`;
+    };
+    document.getElementById("player").style.width = "0px";
+    document.querySelector("#chatContainer").insertAdjacentHTML(
+      "afterbegin",
+      `<div class="mod-head-side-bar-container">
+         <div class="overflow-container-header">
+<ul class="header-wrapper" style="display: flex;display: flex;flex-direction: row;align-items: center;background-color: rgb(0, 37, 105);padding: 0;list-style: none;height: 4rem;border: 0.5px solid #ccc;border-left: 0;">
+                  <div><img class="sidebar-img-icon" src="https://www.chatbotsportal.com/api/chat/get-file/60a74746edb96a69de78af7e" style="width: 37px; height: auto;margin-left: 10px;margin-right: 10px;vertical-align: middle;"></div><div class="header-title" id="main-header-title" style="font-weight: 400;font-size: medium;color: white;font-family: 'botplatform-font';display: flex;flex-direction: column;margin-left: 0px;">
+                  TAMI - UAT<div style="/* margin-left: 58px; */font-size: small;">TA Messaging Intelligence</div></div>
+                   <div id="fadeshow">
+                   <div id="e11" class="icons-box" style="padding: 0 1rem;"> 
+                       <span class="header-bar-icon-box" title="minimize" style="color: white;font-weight: 900;font-size: 30px;width: 100%;padding-left: 0.5rem;cursor: pointer;">-</span>
+                   </div>
+                   <div id="e10" style="cursor:pointer;"> 
+                   <span class="header-bar-icon-box" title="maxamize"><img src="https://cdn.yellowmessenger.com/m65zoVM7Si3s1624952263631.png" style="width: 1.7rem; height: 20px;"></span>
+                   </div>
+                   <div id="e9" class="icons-box" padding:="" 0="" 1rem;=""> 
+                       <span class="header-bar-icon-box" title="Close" style="padding: 0 1rem;color: white;font-weight: 900;font-size: 20px;width: 100%;cursor: pointer;">x</span>
+                   </div>
+              </div>
+             </ul>
+          </div> 
+      </div> `
+    );
+  } catch (e) {
+    console.log("failing while trying to insert add frame script", e);
+  }
+})();
+
+function dynamicHeaderTitle(e) {
+  e.preventDefault();
+  var title = e.srcElement.getAttribute("title");
+}
+document.getElementById("e9").onclick = function () {
+  parent.postMessage("CLOSE", "*");
+};
+
+document.getElementById("e10").onclick = function () {
+  parent.postMessage("Maxamize", "*");
+};
+
+document.getElementById("e11").onclick = function () {
+  parent.postMessage("MINIMIZE", "*");
+};
